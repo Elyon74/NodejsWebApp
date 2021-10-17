f4e777857f7c9c39989da71ad7d787fa5e9ff199
Elyon74 <48656046+Elyon74@users.noreply.github.com>
2021-10-17 13:49:57 +0200

    1.0

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ var port = process.env.PORT || 1337;
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFile('./index.html', null, (erreur, data) => {
+    fs.readFile('./index.html', null, (erreur, html) => {
         if (erreur) {
-            res.end('Erreur')
-        } else {
-            res.end(data)
-        }
-    })
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Serveur lancer ' + 'sur le port ' +res.connection.localPort +' .');
-}).listen(port);
+            res.writeHeader(200, { 'Content-Type': 'text/html' });
+            response.write(html);
+            res.end('Serveur lancer ' + 'sur le port ' + res.connection.localPort + ' .');
+        }).listen(port);
+});
